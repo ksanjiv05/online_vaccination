874e89925a398bab0f541524f57b0b7ec21045b2
sanjiv kumar <ksanjiv0005@gmail.com>
2020-07-12 10:14:33 +0530

    update app

--- app.js
+++ app.js
@@ -6,24 +6,24 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 app.use(cors());
 
-const User = require("./models/usermodel");
+//const User = require("./models/usermodel");
 
 app.use(myParser.urlencoded({ extended: true }));
 app.use(myParser.json({ extended: true })); // need if you want to json data from request
 app.use(cookieParser());
 const port = process.env.Port || 3001;
 
-app.use("/api", require("./routes/common.js"));
+//app.use("/api", require("./routes/common.js"));
 
 app.get("/", (req, res) => {
   console.log("welcome sanjiv");
   res.send("Welcome to rest api using nodemon");
 });
 
-app.post("/register", (req, res) => {
-  const reg = new User(req.body);
-  res.send(reg);
-});
+// app.post("/register", (req, res) => {
+//   const reg = new User(req.body);
+//   res.send(reg);
+// });
 
 app.listen(port, () => {
   console.log(`the app listen at port ${port}`);
