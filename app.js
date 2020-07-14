7ced502ee9f02a6921708f9715020b9618ed6bf1
sanjiv kumar <ksanjiv0005@gmail.com>
2020-07-14 10:40:33 +0530

    new update

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(myParser.urlencoded({ extended: true }));
 app.use(myParser.json({ extended: true })); // need if you want to json data from request
 app.use(cookieParser());
-const port = process.env.Port || 3005;
+const port = process.env.Port || 3001;
 
 app.use("/api", require("./routes/common.js"));
 
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
 //   res.send(reg);
 // });
 
-// app.listen(port, () => {
-//   console.log(`the app listen at port ${port}`);
-// });
-app.listen(process.env.PORT || 5000);
+app.listen(port, () => {
+  console.log(`the app listen at port ${port}`);
+});
+//app.listen(process.env.PORT || 3001);
